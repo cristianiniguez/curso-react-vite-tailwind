f942ebb422f69d84dff5a3e3ea1d9d35d45b1b64
cristianiniguez <felizpc1@gmail.com>
2023-06-05 10:52:09 -0400

    enrutamiento y estructura base: analisis de rutas y componentes en react

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+import Home from './pages/Home';
+import MyAccount from './pages/MyAccount';
+import MyOrders from './pages/MyOrders';
+import MyOrder from './pages/MyOrder';
+import SignIn from './pages/SignIn';
+import NotFound from './pages/NotFound';
+
+const Routes = () =>
+  useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '*', element: <NotFound /> },
+  ]);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
