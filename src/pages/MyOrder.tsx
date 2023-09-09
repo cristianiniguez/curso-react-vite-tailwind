d95aa73e8ed8dc12dc622bff3dbcb9bf680ddecc
cristianiniguez <felizpc1@gmail.com>
2023-09-09 13:54:39 -0400

    checkout y ordenes de compra - pagina de myorder: orden individual

--- src/pages/MyOrder.tsx
+++ src/pages/MyOrder.tsx
@@ -1,18 +1,21 @@
+import { useParams } from 'react-router-dom';
 import { useShoppingCart } from '../context';
 import Layout from '../components/Layout';
 import OrderProductCard from '../components/OrderProductCard';
 
 const MyOrder = () => {
   const { orders } = useShoppingCart();
-  const [lastOrder] = orders.slice(-1);
+  const { orderId = '' } = useParams();
 
-  if (!lastOrder) return null;
+  const order =
+    orderId === 'last' ? orders.slice(-1)[0] : orders.find(order => order.id === +orderId);
+  if (!order) return null;
 
   return (
     <Layout>
       <div className='max-w-sm mx-auto'>
-        <h1 className='text-xl text-center mb-4'>My Last Order</h1>
-        {lastOrder.products.map(product => (
+        <h1 className='text-xl text-center mb-4'>My Order ({orderId})</h1>
+        {order.products.map(product => (
           <OrderProductCard key={product.id} product={product} />
         ))}
       </div>
